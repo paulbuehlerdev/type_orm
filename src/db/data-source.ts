f2c418f7660e52efe78a6be4a7858e86a9eaddd4
ton77v <50094687+ton77v@users.noreply.github.com>
2024-02-08 12:14:48 +0700

    Implemented Encryption with better-sqlite3-multiple-ciphers

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,13 +1,36 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm";
+import { Database as BetterSQLDB3} from "better-sqlite3-multiple-ciphers";
+
 import { DbConfig } from "./entities/DbConfig";
+import * as console from "console";
+
+
+// constants will be params in init function | key as partial/closure
+const dbPath = './database/demo.sqlite';
+const encryptionKey = 'your-encryption-key-here';
+
+
+function prepareDB (db: BetterSQLDB3) {
+    // Access the better-sqlite3 Database object directly to set encryption
+    console.log('\n\nPreparing the DB...')
+    // turned out could use "key" for encryption as well
+    db.pragma(`key = '${encryptionKey}'`);
+    // should catch "wrong key" error here if the key is wrong if we set WAL manually
+    // db.pragma('journal_mode = WAL'); // where are sqlite-wal/shm files?
+}
 
 export const AppDataSource = new DataSource({
-    type: "sqlite",
-    database: "database.sqlite",
+    type: 'better-sqlite3', // customize it to use better-sqlite3-multiple-ciphers
+    database: dbPath,
+    driver: require('better-sqlite3-multiple-ciphers'), // 🔑 to use encryption!
     synchronize: true,  // auto-reflect for prototyping
-    logging: false,
+    logging: true,
+    verbose: console.log,
     entities: [DbConfig],
     migrations: [],
     subscribers: [],
+    enableWAL: true, // with this we'll need 2 wrap it all to catch encr. key error
+    // will need to set-up callable with encryption key bound
+    prepareDatabase: prepareDB  // set PRAGMAs here!
 })
